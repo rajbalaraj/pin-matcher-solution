af5ddf6138bc41fc5bbd74f3da2c771c79b34b7c
rajbalaraj <rajbalasr15@gmail.com>
2023-01-16 01:13:01 +0600

    app-update

--- js/app.js
+++ js/app.js
@@ -21,3 +21,24 @@ document.getElementById('generate-pin').addEventListener('click', function () {
     const displayPinField = document.getElementById('display-pin');
     displayPinField.value = pin;
 });
+
+document.getElementById('calculator').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-numbers');
+    const previousTypedNumber = typedNumberField.value;
+    if (isNaN(number)) {
+        if (number === 'C') {
+            typedNumberField.value = '';
+        }
+        else if (number === '<') {
+            const digits = previousTypedNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNumberField.value = remainingDigits;
+        }
+    }
+    else {
+        const newTypedNumber = previousTypedNumber + number;
+        typedNumberField.value = newTypedNumber;
+    }
+})
