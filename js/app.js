2dd7111c00010cd77bae8e04ff1d032985f4167b
rajbalaraj <rajbalasr15@gmail.com>
2023-01-16 01:10:02 +0600

    app-js-

---
+++ js/app.js
@@ -0,0 +1,23 @@
+function getPin() {
+    const pin = generatePin();
+    const pinString = pin + '';
+    if (pinString.length === 4) {
+        return pin;
+    }
+    else {
+        // console.log('pin not 3 digit found', pin);
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const random = Math.round(Math.random() * 10000);
+    return random;
+}
+
+document.getElementById('generate-pin').addEventListener('click', function () {
+    const pin = getPin();
+    // display Pin
+    const displayPinField = document.getElementById('display-pin');
+    displayPinField.value = pin;
+});
