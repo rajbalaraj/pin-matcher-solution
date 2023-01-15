863028f275a7b8e76f37b9f64054a0de35aa7265
rajbalaraj <rajbalasr15@gmail.com>
2023-01-16 01:15:09 +0600

    app-last-update

--- js/app.js
+++ js/app.js
@@ -42,3 +42,23 @@ document.getElementById('calculator').addEventListener('click', function (event)
         typedNumberField.value = newTypedNumber;
     }
 })
+
+document.getElementById('verify-pin').addEventListener('click', function () {
+    const displayPinField = document.getElementById('display-pin');
+    const currentPin = displayPinField.value;
+
+    const typedNumberField = document.getElementById('typed-numbers');
+    const typedNumber = typedNumberField.value;
+
+    const pinSuccessMessage = document.getElementById('pin-success');
+    const pinFailureMessage = document.getElementById('pin-failure');
+
+    if (typedNumber === currentPin) {
+        pinSuccessMessage.style.display = 'block';
+        pinFailureMessage.style.display = 'none';
+    }
+    else {
+        pinFailureMessage.style.display = 'block';
+        pinSuccessMessage.style.display = 'none';
+    }
+})
